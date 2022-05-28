247cb031090d8337801a60a8e8b0852bd0bbd0d3
Arthur <arthur.bouille@edu.ece.fr>
2022-05-28 15:30:31 +0200

    branche finale

---
+++ Projet/parcourir.php
@@ -0,0 +1,203 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+</head>
+
+<body>
+  <nav class="navbar navbar-expand-lg navbar-light nav">
+    <a class="navbar-brand" href="index.html">
+      <img src="images/logo.png" width="60" height="60" class="" alt="">&nbsp;&nbsp;Omnès Santé
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link d-inline-block align-top" href="index.html">
+              <font size="+1">Acceuil</font>
+            </a>
+          </li>
+          <li class="nav-itema active">
+            <a class="nav-link" href="parcourir.html">
+              <font size="+1">Tout Parcourir</font>
+            </a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href=".php">
+              <font size="+1">Rendez-vous</font>
+            </a>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control" type="search" placeholder="Search" aria-label="Search">
+          <a type="button" class="btn btn-link mr-2 my-sm-0" href="recherche.php"><i class="bi bi-search"></i></a>
+        </form>
+        <a type="button" class="btn btn-success mr-2 my-sm-0" href="client.php"><i class="bi bi-person"></i>&nbsp;Mon
+          compte</a>
+      </div>
+  </nav>
+  <br><br><br>
+  <div class="container">
+    <div class="row">
+      <div class="col-md d-flex justify-content-center">
+        <div class="card cardtrans" stysle="width: 18rem;">
+          <img class="card-img-top" src="images/generaliste.jpg" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Médecin généraliste</h5>
+            <p class="card-text">Touvez le rendez vous qui vous convient le mieux avec un médecin généraliste.</p>
+            <a href="listeMedGen.php" class="btn btn-primary">Trouver mon rendez-vous</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md d-flex justify-content-center">
+        <div class="card cardtrans" style="width: 18rem;">
+          <img class="card-img-top" src="images/specialiste.jpg" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Médecin spécialiste</h5>
+            <p class="card-text">Touvez le rendez vous qui vous convient le mieux avec un médecin spécialiste
+              (Addictologie, cardiologie, dermatologie, etc).</p>
+            <a href="spe.html" class="btn btn-primary">Trouvez ma spécialité</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md d-flex justify-content-center">
+        <div class="card cardtrans" style="width: 18rem;">
+          <img class="card-img-top" src="images/labo.jpg" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Laboratoire de biologie médicale (MAIS LA PHP)</h5>
+            <p class="card-text">Touvez le rendez vous qui vous convient le mieux dans un laboratoire de biologie
+              médicale.</p>
+            <a href="labo.html" class="btn btn-primary">Trouver mon rendez-vous</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+<br><br>
+
+<footer class="text-center text-lg-start bg-light text-muted">
+  <!-- Section: Social media -->
+  <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+    <!-- Right -->
+    <div>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-facebook-f"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-twitter"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-google"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-instagram"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-linkedin"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-github"></i>
+      </a>
+    </div>
+    <!-- Right -->
+  </section>
+
+  <!-- Section: Links  -->
+  <section class="">
+    <div class="container text-center text-md-start mt-5">
+      <!-- Grid row -->
+      <div class="row mt-3">
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <!-- Content -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            <i class="fas fa-gem me-3"></i>Company name
+          </h6>
+          <p>
+            Here you can use rows and columns to organize your footer content. Lorem ipsum
+            dolor sit amet, consectetur adipisicing elit.
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Products
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">Angular</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">React</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Vue</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Laravel</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Useful links
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">Pricing</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Settings</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Orders</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Help</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Contact
+          </h6>
+          <p><i class="fas fa-home me-3"></i> New York, NY 10012, US</p>
+          <p>
+            <i class="fas fa-envelope me-3"></i>
+            info@example.com
+          </p>
+          <p><i class="fas fa-phone me-3"></i> + 01 234 567 88</p>
+          <p><i class="fas fa-print me-3"></i> + 01 234 567 89</p>
+        </div>
+        <!-- Grid column -->
+      </div>
+      <!-- Grid row -->
+    </div>
+  </section>
+  <!-- Section: Links  -->
+
+  <!-- Copyright -->
+  <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+    © 2021 Copyright:
+    <a class="text-reset fw-bold" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+
+</html>
